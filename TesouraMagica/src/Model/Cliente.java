d4863b30f25f7ba1ac8509caf0dc25b3ce91002a
Juliana Lobo <32516456+xolis@users.noreply.github.com>
2020-11-18 23:16:55 -0300

    Ajustes Classes

--- TesouraMagica/src/Model/Cliente.java
+++ TesouraMagica/src/Model/Cliente.java
@@ -11,109 +11,28 @@ import java.util.Date;
  *
  * @author 55349
  */
-public class Cliente
+public class Cliente extends Pessoa
 {
-    
-    private int id;
-    private String nome;
-    private char sexo;
-    private Date dataNascimento;
-    private String telefone;
-    private String email;
-    private String rg;
     private String endereco;
-    private String cep;
+    private String cep; 
 
-    public Cliente(int id, String nome, char sexo, Date dataNascimento, String telefone, String email, String endereco, String cep)
+    public Cliente(String endereco, String cep, int id, String nome, char sexo, Date dataNascimento, String telefone, String email, String rg)
     {
-        this.id = id;
-        this.nome = nome;
-        this.sexo = sexo;
-        this.dataNascimento = dataNascimento;
-        this.telefone = telefone;
-        this.email = email;
+        super(id, nome, sexo, dataNascimento, telefone, email, rg);
         this.endereco = endereco;
         this.cep = cep;
     }
 
-    public Cliente(int id, String nome, char sexo, String telefone, String endereco)
+    public Cliente(String endereco, String cep, int id, String nome)
     {
-        this.id = id;
-        this.nome = nome;
-        this.sexo = sexo;
-        this.telefone = telefone;
+        super(id, nome);
         this.endereco = endereco;
+        this.cep = cep;
     }
-
-    public int getId()
-    {
-        return id;
-    }
-
-    public void setId(int id)
-    {
-        this.id = id;
-    }
-
-    public String getNome()
-    {
-        return nome;
-    }
-
-    public void setNome(String nome)
-    {
-        this.nome = nome;
-    }
-
-    public char getSexo()
-    {
-        return sexo;
-    }
-
-    public void setSexo(char sexo)
-    {
-        this.sexo = sexo;
-    }
-
-    public Date getDataNascimento()
-    {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(Date dataNascimento)
-    {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public String getTelefone()
-    {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone)
-    {
-        this.telefone = telefone;
-    }
-
-    public String getEmail()
-    {
-        return email;
-    }
-
-    public void setEmail(String email)
-    {
-        this.email = email;
-    }
-
-    public String getRg()
-    {
-        return rg;
-    }
-
-    public void setRg(String rg)
-    {
-        this.rg = rg;
-    }
+    
+    
+    
+    
 
     public String getEndereco()
     {
@@ -136,5 +55,7 @@ public class Cliente
     }
     
     
+    
+
     
 }
--- TesouraMagica/src/Model/Usuario.java
+++ TesouraMagica/src/Model/Usuario.java
@@ -11,109 +11,23 @@ import java.util.Date;
  *
  * @author 55349
  */
-public class Usuario
+public class Usuario extends Pessoa
 {
     
-    private int id;
-    private String nome;
-    private char sexo;
-    private Date dataNascimento;
-    private String telefone;
-    private String email;
-    private String rg;
-    private String endereco;
-    private String senha;
-    private String nivelAcesso;
+    protected String senha;
+    protected String nivelAcesso;
 
-    public Usuario(int id, String nome, char sexo, Date dataNascimento, String telefone, String email, String rg, String endereco, String senha, String nivelAcesso)
+    public Usuario(String senha, int id, String nome)
     {
-        this.id = id;
-        this.nome = nome;
-        this.sexo = sexo;
-        this.dataNascimento = dataNascimento;
-        this.telefone = telefone;
-        this.email = email;
-        this.rg = rg;
-        this.endereco = endereco;
+        super(id, nome);
+        this.senha = senha;
+    }
+
+    public Usuario(String senha, String nivelAcesso, int id, String nome, char sexo, Date dataNascimento, String telefone, String email, String rg)
+    {
+        super(id, nome, sexo, dataNascimento, telefone, email, rg);
         this.senha = senha;
         this.nivelAcesso = nivelAcesso;
-    }
-
-    public Usuario(int id, String nome, String senha)
-    {
-        this.id = id;
-        this.nome = nome;
-        this.senha = senha;
-    }
-
-    public String getNome()
-    {
-        return nome;
-    }
-
-    public void setNome(String nome)
-    {
-        this.nome = nome;
-    }
-
-    public char getSexo()
-    {
-        return sexo;
-    }
-
-    public void setSexo(char sexo)
-    {
-        this.sexo = sexo;
-    }
-
-    public Date getDataNascimento()
-    {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(Date dataNascimento)
-    {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public String getTelefone()
-    {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone)
-    {
-        this.telefone = telefone;
-    }
-
-    public String getEmail()
-    {
-        return email;
-    }
-
-    public void setEmail(String email)
-    {
-        this.email = email;
-    }
-
-    public String getRg()
-    {
-        return rg;
-    }
-
-    public void setRg(String rg)
-    {
-        this.rg = rg;
-    }
-
-    public String getEndereco()
-    {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco)
-    {
-        this.endereco = endereco;
     }
 
     public String getSenha()
@@ -135,6 +49,7 @@ public class Usuario
     {
         this.nivelAcesso = nivelAcesso;
     }
+
     
     
     
